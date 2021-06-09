55d90c715d2668066f29db5f9b26f187d59a4162
MaryG2319 <77905928+MaryG2319@users.noreply.github.com>
2021-06-09 08:18:40 +0100

    Add files via upload

---
+++ Code/ES_PL3_2191258_2182219_2181459_2182306/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/Inscrever.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Inscrever {
+    private JPanel increver;
+    private JTextPane textPane1;
+    private JTextPane textPane2;
+    private JTextPane textPane3;
+    private JButton voltarButton;
+    private JButton concluirButton;
+}
---
+++ Code/ES_PL3_2191258_2182219_2181459_2182306/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/Provas.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static javax.swing.WindowConstants.DISPOSE_ON_CLOSE;
+
+public class Provas extends JFrame{
+    private JTextPane textPane1;
+    private JButton btn_insc;
+    private JButton btn_importar;
+    private JButton btn_voltar;
+    private JButton btn_detalhes;
+    private JButton btn_novo;
+    private JPanel provas;
+
+    public Provas(){
+        setContentPane(provas);
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setVisible(true);
+        pack();
+
+        btn_voltar.addActionListener(this::btn_voltarActionPerformed);
+    }
+
+    private void btn_voltarActionPerformed(ActionEvent actionEvent) {
+        new Main();
+        setVisible(false);
+    }
+}
