120fd684231d004c1de17c7bfc18748ea52bcc47
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-22 02:18:27 +0100

    Update model classes (evento  e prova)

--- Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/model/Evento.java
+++ Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/model/Evento.java
@@ -1,21 +1,57 @@
 package model;
 
-import java.sql.Date;
+import java.util.Date;
 
 public class Evento {
     private int id;
     private String nome;
     private Date inicio;
     private Date fim;
-    private String local;
     private String pais;
+    private String local;
 
     public Evento() {
     }
 
+    public Evento(String nome, Date inicio, Date fim, String pais, String local) {
+        this.nome = nome;
+        this.inicio = inicio;
+        this.fim = fim;
+        this.local = local;
+        this.pais = pais;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Date getInicio() {
+        return inicio;
+    }
+
+    public long getInicioTime() {
+        return inicio.getTime();
+    }
+
+    public Date getFim() {
+        return fim;
+    }
+
+    public long getFimTime() {
+        return fim.getTime();
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public String getLocal() {
+        return local;
+    }
+
     @Override
     public String toString() {
+        return id + " inicio " + inicio + " fim " + fim;
         //todo
-        return super.toString();
     }
 }
