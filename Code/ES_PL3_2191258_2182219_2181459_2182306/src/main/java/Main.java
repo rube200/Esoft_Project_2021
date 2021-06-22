f5dc7beea82405b3cf1090bda07f81e7df6738be
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-22 02:19:52 +0100

    Update main to work with last api

--- Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/Main.java
+++ Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/Main.java
@@ -1,34 +1,48 @@
+import API.CrudController;
 import API.DatabaseConnector;
-import API.MainView;
-import com.google.inject.AbstractModule;
-import com.google.inject.Guice;
-import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.spi.InjectionPoint;
+import API.ViewBase;
+import com.google.inject.*;
+import com.google.inject.name.Names;
 import controllers.DatabaseQuery;
+import controllers.EventosController;
+import controllers.ProvasController;
+import controllers.ViewController;
+import model.Evento;
+import model.Prova;
+import views.MainFrame;
+import views.Provas;
 import views.WorldAthletics;
+import views.eventos.Eventos;
 
 import javax.swing.*;
 
 class Main extends AbstractModule {
+    @Inject
+    ViewController viewController;
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            Injector injector = Guice.createInjector(new Main());
-            injector.getInstance(Main.class).start();
-        });
+        Injector injector = Guice.createInjector(new Main());
+        injector.getInstance(Main.class).start();
     }
 
     @Override
     protected void configure() {
+        //bind controllers
         bind(DatabaseConnector.class).to(DatabaseQuery.class);
-        bind(MainView.class).to(WorldAthletics.class);
+        bind(new TypeLiteral<CrudController<Evento>>() {
+        }).annotatedWith(Names.named("EventosController")).to(EventosController.class);
+        bind(new TypeLiteral<CrudController<Prova>>() {
+        }).annotatedWith(Names.named("ProvasController")).to(ProvasController.class);
+        bind(ViewController.class);
+
+        //bind views
+        bind(JFrame.class).to(MainFrame.class);
+        bind(ViewBase.class).annotatedWith(Names.named("EventosView")).to(Eventos.class);
+        bind(ViewBase.class).annotatedWith(Names.named("ProvasView")).to(Provas.class);
+        bind(ViewBase.class).annotatedWith(Names.named("WorldAthleticsView")).to(WorldAthletics.class);
     }
 
-    @Inject
-    MainView mainView;
-
     private void start() {
-        mainView.prepareView();
-        mainView.displayView();
+        viewController.initiateProgram();
     }
 }
