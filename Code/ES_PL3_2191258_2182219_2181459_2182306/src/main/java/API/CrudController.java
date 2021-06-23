d850ca0c9456ae5061159e636a426788985c9e19
Rúben Garcia <rube200@users.noreply.github.com>
2021-06-23 18:44:10 +0100

    Update Crud API to support edit and delete

--- Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/API/CrudController.java
+++ Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/API/CrudController.java
@@ -4,11 +4,15 @@ public interface CrudController<T> {
     //todo limitar listas
     //todo permitir enter para guardar
 
+    void destroy(T data);
+
     T create();
 
     void edit(T data);
 
     void index();
+
+    void update(T data);
 
     void store(T data);
 
--- Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/API/DatabaseConnector.java
+++ Code/ES_PL3_2191258_2182219_2181459_2182306/src/main/java/API/DatabaseConnector.java
@@ -7,18 +7,33 @@ import model.Prova;
 import java.util.Collection;
 
 public interface DatabaseConnector {
+    boolean delete(Evento evento);
+
     Collection<Evento> getEventos();
+
     Collection<Evento> getEventosAtuais();
+
     Collection<Evento> getEventoAtuaisOuFuturos();
+
     boolean store(Evento evento);
+
     boolean update(Evento evento);
 
+    boolean delete(Prova prova);
+
     Collection<Prova> getProvas();
+
     Collection<Prova> getProvasAtuais();
+
     boolean store(Prova prova);
+
     boolean update(Prova prova);
 
+    boolean delete(Modalidade modalidade);
+
     Collection<Modalidade> getModalidades();
+
     boolean store(Modalidade modalidade);
+
     boolean update(Modalidade modalidade);
 }
